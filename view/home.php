3fc6ee7e7493489089720b63be7b2560b5196bb0
farhin <farhinzahra012@gmail.com>
2025-03-09 21:16:59 +0600

    habijabi

---
+++ view/home.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+    <style>
+        table, th, td {
+            border: 1px solid black;
+            
+            padding: 10px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Hi!</h1>
+    <table>
+        <tr>
+            <th><img src="http://localhost/springwtj/images/images (1).jpg" alt="Bird1" width="200"></th>
+            <th>bird2</th>
+            <th>Bird3</th>
+        </tr>
+        <tr>
+            <td>Peacock</td>
+            <td>Sparrow</td>
+            <td>Parrot</td>
+        </tr>
+    </table>
+</body>
+</html>
